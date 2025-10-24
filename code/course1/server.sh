85d815819a86b4359eca6c56e8d0e9eeb1239cc2
fushenshen <hellofss@foxmail.com>
2025-10-25 00:30:42 +0800

    增加配置

---
+++ code/course1/server.sh
@@ -0,0 +1,9 @@
+http_proxy= https_proxy= no_proxy=* python3 -m vllm.entrypoints.openai.api_server \
+  --model "Qwen/Qwen3-1.7B" \
+  --dtype float16 \
+  --max-model-len 4096 \
+  --gpu-memory-utilization 0.95 \
+  --max-num-batched-tokens 8192 \
+  --max-num-seqs 256 \
+  --port "13337"    \
+  --enforce-eager
